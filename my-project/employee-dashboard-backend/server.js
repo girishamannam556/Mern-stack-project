c43763f1e3b76da6542a724f3c1a5a86ec966522
Girisha <mannamgirisha@gmail.com>
2026-02-11 17:27:54 +0530

    ready for deployment

--- my-project/employee-dashboard-backend/server.js
+++ my-project/employee-dashboard-backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const PORT = process.env.PORT || 5000;
 
 require('dotenv').config()
 
@@ -22,6 +23,6 @@ app.use('/api/auth', authRoutes)
 app.use('/api/tasks', taskRoutes)
 
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(PORT, () => {
   console.log('Server running on port 5000')
 })
